05cea0004ecdf3138c93f4871a09bcfd21990714
Eliza Fury <39455919+Efury1@users.noreply.github.com>
2023-07-30 02:48:19 +1000

    Rewrite base

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./index.html"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
