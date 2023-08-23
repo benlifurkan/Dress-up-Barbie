d41821e361eafb2eef045cbe7beea47f6cedc520
Eliza Fury <39455919+Efury1@users.noreply.github.com>
2023-08-23 21:30:11 +1000

    Add title

--- barbie-dress-up/src/App.tsx
+++ barbie-dress-up/src/App.tsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   return (
     <>
-      
+    <title>Barbie Dress up Game</title>
+    <link rel="stylesheet" href="style.css"></link>
     </>
   )
 }
